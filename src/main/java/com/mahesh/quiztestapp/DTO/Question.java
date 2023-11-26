feae06d71fa0efadf267ad4086cbb5c8702cd6e2
Maheshkale96 <maheshkale11196@gmail.com>
2023-11-26 22:41:12 +0530

    property changed for id column

--- src/main/java/com/mahesh/quiztestapp/DTO/Question.java
+++ src/main/java/com/mahesh/quiztestapp/DTO/Question.java
@@ -11,7 +11,7 @@ import jakarta.persistence.Id;
 public class Question {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
 	
